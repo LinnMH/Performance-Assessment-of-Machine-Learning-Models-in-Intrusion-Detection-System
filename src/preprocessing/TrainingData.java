e04aaeb0a24b54068454ba4dd5b270cb6e21fbd9
cli63 <cli63@nyit.edu>
2024-09-24 17:26:52 -0700

    Initial files

---
+++ src/preprocessing/TrainingData.java
@@ -0,0 +1,8 @@
+package preprocessing;
+
+public class TrainingData {
+
+    public void extractData(String originalFile, String trainingFile, String testFile) {
+        // Read originalFile, extract parts of data and save to trainingFile
+    }
+}
---
+++ src/training/Model.java
@@ -0,0 +1,12 @@
+package training;
+
+public class Model {
+
+    public void trainModel(String trainingFile, String trainingModel, String modelFile) {
+        // Train model with trainingFile, and trainingModel, output model to modelFile
+    }
+
+    public void testModel(String testFile, String trainingModel, String modeFile, String resultFile) {
+        // Test testFile data with training Model and modelFile, output result to resultFile
+    }
+}
